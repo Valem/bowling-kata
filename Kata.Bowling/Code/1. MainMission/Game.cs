4059349b2df70406b688dc3d11952ab87b34282a
Luciano Briganti <luciano.briganti@wincasa.ch>
2023-08-27 17:32:51 +0200

    MainMissionSolution1

--- Kata.Bowling/Code/1. MainMission/Game.cs
+++ Kata.Bowling/Code/1. MainMission/Game.cs
@@ -2,7 +2,47 @@
 
 public class Game
 {
-    /*
-     * Put your magic spells here for the main mission.
-     */
+    private readonly List<int> _rolls = new List<int>();
+
+    public void Roll(int pins)
+    {
+        _rolls.Add(pins);
+    }
+
+    public int Score()
+    {
+        int score = 0;
+        int frameIndex = 0;
+
+        for (int frame = 0; frame < 10; frame++)
+        {
+            if (IsStrike(frameIndex))
+            {
+                score += 10 + StrikeBonus(frameIndex);
+                frameIndex++;
+            }
+            else if (IsSpare(frameIndex))
+            {
+                score += 10 + SpareBonus(frameIndex);
+                frameIndex += 2;
+            }
+            else
+            {
+                score += SumOfPinsInFrame(frameIndex);
+                frameIndex += 2;
+            }
+        }
+
+        return score;
+    }
+
+    private bool IsStrike(int frameIndex) => _rolls[frameIndex] == 10;
+
+    private bool IsSpare(int frameIndex) => _rolls[frameIndex] + _rolls[frameIndex + 1] == 10;
+
+    private int StrikeBonus(int frameIndex) => _rolls[frameIndex + 1] + _rolls[frameIndex + 2];
+
+    private int SpareBonus(int frameIndex) => _rolls[frameIndex + 2];
+
+    private int SumOfPinsInFrame(int frameIndex) => _rolls[frameIndex] + _rolls[frameIndex + 1];
 }
--- Kata.Bowling/Test/1. MainMission/BowlingSagaShould.cs
+++ Kata.Bowling/Test/1. MainMission/BowlingSagaShould.cs
@@ -1,5 +1,6 @@
-using Kata.Bowling.Code._1._MainMission;
+using Shouldly;
 using Xunit.Abstractions;
+using Game = Kata.Bowling.Code._1._MainMission.Game;
 
 namespace Kata.Bowling.Test._1._MainMission
 {
@@ -10,23 +11,91 @@ namespace Kata.Bowling.Test._1._MainMission
          */
 
         private readonly ITestOutputHelper _outputHelper;
+        private readonly Game _game;
 
         public BowlingSagaShould(ITestOutputHelper outputHelper)
         {
             _outputHelper = outputHelper;
+            _game = new Game();
         }
 
         [Fact]
-        public async Task TestFoo_Withxx_Expectxx()
+        public void Score_WhenGutterGame_ShouldReturnZero()
         {
             // Arrange
-            var testee = new Game();
+            RollMany(20, 0);
 
             // Act
-            //var result = await testee.DoIt();
+            var score = _game.Score();
 
             // Assert
-            //result.ShouldNotBeNull();
+            score.ShouldBe(0);
+        }
+
+        [Fact]
+        public void Score_WhenOnePinIsKnockedDownOnEachRoll_ShouldReturn20()
+        {
+            // Arrange
+            RollMany(20, 1);
+
+            // Act
+            var score = _game.Score();
+
+            // Assert
+            score.ShouldBe(20);
+        }
+
+        [Fact]
+        public void Score_WhenAllStrikes_ShouldReturn300()
+        {
+            // Arrange
+            RollMany(12, 10);
+
+            // Act
+            var score = _game.Score();
+
+            // Assert
+            score.ShouldBe(300);
+        }
+
+        [Fact]
+        public void Score_ShouldAccountForOneSpare()
+        {
+            // Arrange
+            _game.Roll(5);
+            _game.Roll(5);  // Spare
+            _game.Roll(3);
+            RollMany(17, 0);
+
+            // Act
+            var score = _game.Score();
+
+            // Assert
+            score.ShouldBe(16);
+        }
+
+        [Fact]
+        public void Score_ShouldAccountForOneStrike()
+        {
+            // Arrange
+            _game.Roll(10);  // Strike
+            _game.Roll(3);
+            _game.Roll(4);
+            RollMany(16, 0);
+
+            // Act
+            var score = _game.Score();
+
+            // Assert
+            score.ShouldBe(24);
+        }
+
+        private void RollMany(int n, int pins)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                _game.Roll(pins);
+            }
         }
     }
 }
